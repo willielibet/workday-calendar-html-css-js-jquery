55b7a756f4991bf69ca6dbc7844e0ede5b5b4ee4
Willie <write2willie@gmail.com>
2021-03-15 18:50:06 -0400

    commit #6: coding local storage to save it and display it, march152021, 6.50pm

--- js/script.js
+++ js/script.js
@@ -18,9 +18,9 @@ let currentTime = moment().format("H");
         //console.log("currentTime in document line 18 " + currentTime)
         //console.log("timeArray document line 125 " + timeArray)
         //console.log("indexOf line 130" + timeArray.indexOf(currentTime));
-    //for (i=0; i < 9; i++) {
-        let i=0;
-        while (i<9) {
+    for (i=0; i < 9; i++) {
+        //let i=0;
+        //while (i<9) {
 
         //console.log("currentTime in document line 24 " + currentTime)
         //console.log("indexOf in document line 24 " + timeArray.indexOf(1))
@@ -30,20 +30,20 @@ let currentTime = moment().format("H");
            // $(`.TextBoxColors-${timeArray[i]}`).css('background-color', 'gray')
         //}
         if (currentTime > parseInt(timeArray[0])) {
-            $(`.TextBoxColors-${timeArray[0]}`).css('background-color', 'gray')
+            $(`.TextBoxColors-${timeArray[0]}`).css('background-color', 'Gray')
         }
         else if (currentTime === timeArray[0]) {
             $(`.TextBoxColors-${currentTime}`).css('background-color', 'Tomato')
         }
         else if (currentTime < timeArray[0]) {
-            $(`.TextBoxColors-${timeArray[i]}`).css('background-color', 'green')
+            $(`.TextBoxColors-${timeArray[i]}`).css('background-color', 'MediumSeaGreen')
         }
         else {
         }
 
         console.log("timeArray l-30 " + timeArray[i])
         if (currentTime > timeArray[i]) {
-            $(`.TextBoxColors-${timeArray[i]}`).css('background-color', 'gray')
+            $(`.TextBoxColors-${timeArray[i]}`).css('background-color', 'Gray')
             console.log("currentTime l-32 " + currentTime)
             console.log("timeArray l-33 " + timeArray[i])
         } 
@@ -55,9 +55,45 @@ let currentTime = moment().format("H");
             // console.log("timeArray l34 " + timeArray[i])
             // console.log("currentTime l-42 " + currentTime)
             // console.log("timeArray l-42 " + timeArray[i])
-            $(`.TextBoxColors-${timeArray[i]}`).css('background-color', 'green')
+            $(`.TextBoxColors-${timeArray[i]}`).css('background-color', 'MediumSeaGreen')
         } 
         else {
         }
-        i++
+        //i++
 }
+
+
+// $("textarea").on("click", function(){
+//     $('textarea').each(function() {
+//      var value = $(this).val(),
+//          todo = $(this).attr('todo');
+//      localStorage[todo] = value;
+             
+// })});
+
+// $("btn btn-primary").on("keyup", function () {
+//     // $('textarea').each(function() {
+//     localStorage.setItem("item" + $(this).attr("btn btn-primary"), $(this).val());
+//      //var value = $(this).val(),
+//        //  todo = $(this).attr('todo');
+//      //localStorage[todo] = value;
+//      //localStorage.setItem("item", JSON.stringify(localS));
+             
+// //})
+// });
+
+//var savedTodo = JSON.parse(localStorage.getItem("todo"))
+//$(".TextBoxColors-17").val(savedTodo)
+
+
+myLoad()
+
+function mySave() {
+    var myContent = document.getElementById("TextBoxStorage-17").value;
+    localStorage.setItem("myContent", myContent);
+  }
+
+function myLoad() {
+    var myContent = localStorage.getItem("myContent");
+    document.getElementById("TextBoxStorage-17").value = myContent;
+  }
