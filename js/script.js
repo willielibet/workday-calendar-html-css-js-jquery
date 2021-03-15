54946a9cd08299158b6e598c22bed6ffcd17b015
Willie <write2willie@gmail.com>
2021-03-15 04:03:11 -0400

    commit #3: add code to compare time and make hour-text field red, green or gray depending on the current time, march152021, 4.03am

--- js/script.js
+++ js/script.js
@@ -9,18 +9,46 @@ $(document).ready(function(){
 var timeArray = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
 var currentTime = moment().format("h A");
 
-//gives the current time to the hour. for example 11 PM.
+
+//gives the current time to the hour for each span element that contains 
+//the time in string format. for example 11A M.
 //The each() method specifies a function to run for each matched element.
+//element - The current element (the "this" selector can also be used).
 //source: https://www.w3schools.com/jquery/misc_each.asp.
-//Syntax:  $(selector).each(function(index,element))
+//Syntax:  $(selector).each(function(index,element))/
 $(".TIME").each(function(i){
     //current hour (see line 5 to display it in the browser).
    $(this).text(moment().hour(i+9).format("h A"));
     
 })
 
-// console.log(currentTime)
-//console.log("value of each function " + each())
+$(document).ready(function(){
+    
+    $.each( timeArray, function(value) {
+        console.log("currentTime in document line 29 " + currentTime)
+        console.log("timeArray document line 29 " + timeArray)
+
+        if (currentTime < timeArray[value]) {
+            console.log("currentTime in document line 30 " + currentTime)
+            //alert("yes!")
+        $('.TextBox').css('background-color', 'green')
+
+        if (currentTime === timeArray[value]) {
+            console.log("currentTime in document line 30 " + currentTime)
+            //alert("yes!")
+        $('.TextBox').css('background-color', 'red')
+        }
+
+        if (currentTime > timeArray[value]) {
+            console.log("currentTime in document line 30 " + currentTime)
+            //alert("yes!")
+        $('.TextBox').css('background-color', 'gray')
+        }
+        }
+
+    });
+});
+
     
 
 
