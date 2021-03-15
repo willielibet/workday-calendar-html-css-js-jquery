2fcef3770c98287528b677fd4f77bff402586205
Willie <write2willie@gmail.com>
2021-03-15 03:11:29 -0400

    commit #2: coding the current time to compare it with an array of strings with elements from 9 to 5, march152021, 3.11am

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
-// var todayDate = moment().format('dddd, MMM Do YYYY');
-// $("#currentDay").html(todayDate);
 
 $(document).ready(function(){
-    /* Display the current day of the week and date
-    at the top of the page */
+    /* shows current day of the week, month and day of the month in the browser */
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    // $("#currentDay").text(moment().hour(1+9).format("h A"));
     
 })
+
+var timeArray = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
+var currentTime = moment().format("h A");
+
+//gives the current time to the hour. for example 11 PM.
+//The each() method specifies a function to run for each matched element.
+//source: https://www.w3schools.com/jquery/misc_each.asp.
+//Syntax:  $(selector).each(function(index,element))
+$(".TIME").each(function(i){
+    //current hour (see line 5 to display it in the browser).
+   $(this).text(moment().hour(i+9).format("h A"));
+    
+})
+
+// console.log(currentTime)
+//console.log("value of each function " + each())
+    
+
+
